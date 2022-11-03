2c1a3bf0cf16a396bd31129a236c686acf5da992
Anit <anittankandasamy@gmail.com>
2022-11-03 02:08:55 -0400

    created Empty component, stories and refactor

--- stories/index.js
+++ stories/index.js
@@ -6,12 +6,17 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
+
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+
 
 
 storiesOf("Button", module)
@@ -143,3 +148,4 @@ storiesOf("Button", module)
           .add("Appointment", () => <Appointment />)
           .add("Appointment with Time", () => <Appointment time="12pm" /> )
           .add("Header", () => <Header time="12pm" />)
+          .add("Empty", () => <Empty onAdd={action("onAdd")} />)
