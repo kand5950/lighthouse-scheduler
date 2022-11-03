f8e6f8a059c9815d31a4f53fe59860872f59f38f
Anit <anittankandasamy@gmail.com>
2022-11-03 09:47:25 -0400

    Created From component, stories, default state, controlled component

--- stories/index.js
+++ stories/index.js
@@ -20,6 +20,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 
@@ -171,4 +172,18 @@ storiesOf("Button", module)
             <Error 
               message="Could not delete appointment."
               onClose={action('onClose')}
+            />)
+          .add("Edit", () => 
+            <Form 
+              student="Anit"
+              interviewer={3}
+              interviewers={interviewers}
+              onSave={action('onSave')}
+              onCancel={action('onCancel')}
+            />)
+          .add("Create", () =>
+            <Form 
+              interviewers={interviewers}
+              onSave={action('onSave')}
+              onCancel={action('onCancel')}
             />)
