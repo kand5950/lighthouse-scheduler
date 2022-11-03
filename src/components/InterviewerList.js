1f18bff74dcc1544f708c0ad1694b88efa622e9f
Anit <anittankandasamy@gmail.com>
2022-11-02 23:42:34 -0400

    refactored rendering interviewList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,10 +3,24 @@ import InterviewerListItem from "./InterviewerListItem";
 import 'components/InterviewerList.scss';
 
 export default function InterviewerList(props) {
+
+  const interviewerList = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+          key={interviewer.id}
+          id={interviewer.id}
+          name={interviewer.name}
+          avatar={interviewer.avatar}
+          setInterviewer={props.setInterviewer}
+          selected={interviewer.id === props.interviewer}
+      />
+    );
+  });
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewerList}</ul>
     </section>
   );
   
