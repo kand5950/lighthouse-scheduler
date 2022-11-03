d329358c46816c12378b79f80e0057a4d390ea69
Anit <anittankandasamy@gmail.com>
2022-11-03 02:24:20 -0400

    created Show component & story, refactor

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 
 
@@ -149,3 +150,10 @@ storiesOf("Button", module)
           .add("Appointment with Time", () => <Appointment time="12pm" /> )
           .add("Header", () => <Header time="12pm" />)
           .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+          .add("Show", () => 
+            <Show 
+              student='Lydia Miller-Jones'
+              interviewer={interviewer.name}
+              onEdit={action("onEdit")}
+              onDelete={action("onDelete")}
+              />)
