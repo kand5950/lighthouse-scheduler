e4f524ca037bdc0609b05496a24d56564c582e3a
Anit <anittankandasamy@gmail.com>
2022-11-03 10:28:14 -0400

    implemented reset function and cancel

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,6 +7,18 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+
+  
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -16,19 +28,21 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            onChange={(event) => setStudent(event.target.value)}
             value={student}
+            onChange={event => setStudent(event.target.value)}
+            
           />
         </form>
         <InterviewerList 
           interviewers={props.interviewers}
-          onChange={setInterviewer}
           value={interviewer}
+          onChange={setInterviewer}
+          
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
