031ef1ed8e0e3f0ff5dd9bb2231798ec2a2bec70
Anit <anittankandasamy@gmail.com>
2022-11-05 03:19:39 -0400

    removed appointments object, added empty object in state, made dailyApp empty array and updated map function

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,56 +6,19 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
-
 export default function Application(props) {
   const [state, setState] = useState ({
     day: 'Monday',
-    days: []
-  })
+    days: [],
+    appointments: {}
+  });
+
+  const dailyAppointments = [];
 
   const setDay = day => setState({ ...state, day});
   const setDays = days => setState(prev => ({ ...prev, days}));
 
-  const appointmentList = Object.values(appointments).map(appointment => {
+  const appointmentList = dailyAppointments.map(appointment => {
     return (
       <Appointment 
         key={appointment.id}
