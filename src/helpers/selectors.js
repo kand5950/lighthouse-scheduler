bba9537d7e304e3f33cf42e3df397b1bd42e4a03
Anit <anittankandasamy@gmail.com>
2022-11-05 02:34:31 -0400

    Implemented getAppointmentsForDAy code & passed tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,22 @@
 export function getAppointmentsForDay(state, day) {
   const dayObj = state.days.find(element => element.name === day);
-  return dayObj;
+  
+  const results = [];
+
+  if (!dayObj) {
+    return [];
+  } 
+
+  const appointmentIds = dayObj.appointments
+  const appointmentsInfoForDay = [];
+  
+  for (const id in state.appointments){
+    if (appointmentIds.includes(Number(id))) {
+      appointmentsInfoForDay.push(state.appointments[id]);
+    }
+    
+  }
+  return appointmentsInfoForDay;
+
+
 }
