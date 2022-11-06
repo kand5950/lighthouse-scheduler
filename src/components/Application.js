ccb4b94f500f51df5555f4a5d2559c057a7cb114
Anit <anittankandasamy@gmail.com>
2022-11-06 17:53:20 -0500

    updating appointment component to create a interviews array with new selector and passed down as a prop to form component

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,7 +5,7 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState ({
@@ -28,6 +28,7 @@ export default function Application(props) {
 
   }, [])
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const dailyInterviewers = getInterviewersForDay(state, state.day);
   const appointmentList = dailyAppointments.map(appointment => {
     const interview = getInterview(state, appointment.interview);
 
@@ -36,6 +37,7 @@ export default function Application(props) {
         key={appointment.id}
         {...appointment}
         interview={interview}
+        interviewers={dailyInterviewers}
       />
     )
   }) 
