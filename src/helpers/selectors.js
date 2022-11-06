7f27a9d298c5eb575b408e55b7bdc7f70528f9cb
Anit <anittankandasamy@gmail.com>
2022-11-06 00:13:38 -0400

    implemented getInterview code & passed tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -19,5 +19,22 @@ export function getAppointmentsForDay(state, day) {
 }
 
 export function getInterview(state, interview) {
-  
+
+  if(!interview) {
+    return null;
+  }
+
+  const interviewerId = interview.interviewer
+
+  for (const id in state.interviewers) {
+    if (Number(id) === interviewerId) {
+      return (
+        {
+          student: interview.student,
+          interviewer: state.interviewers[id]
+        }
+      )
+    }
+  }
+
 }
