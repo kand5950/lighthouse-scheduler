daf985a091ed8eff0f0c96fcfa88a64049ea9680
Anit <anittankandasamy@gmail.com>
2022-11-06 04:36:10 -0500

    added optional replace paramater and passed tests. Notes//

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,17 +4,23 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]); //an array of modes
 
-  const transition = newMode => {
+  const transition = (newMode, replace = false) => {
     setMode(newMode)
-    setHistory(history => [...history, newMode])}; //adds newMode to history [initial, newMode]
+
+    if (replace){
+                            //[FIRST, SECOND]   =>     [FIRST]   => [FIRST, THIRD]        
+      setHistory(history => [...history.slice(0, history.length -1), newMode]); 
+    } else {                
+      setHistory(history => [...history, newMode])}; //adds newMode to history [initial, newMode]
+    };
   
     const back = () => {
 
-      if (history.length === 1) return; //doesnt allow user to go past initial mode
-      setHistory(history => history.slice(0, history.length -1)); //makes a new history without last index
-      setMode(history[history.length-2]); //sets the mode to the previous item in history array
+    if (history.length === 1) return; //doesnt allow user to go past initial mode
+    setHistory(history => history.slice(0, history.length -1)); //makes a new history without last index
+    setMode(history[history.length-2]); //sets the mode to the previous item in history array
 
-    };
+  };
 
   return {
     mode,
