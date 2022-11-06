fe7130d32fd175ad5aa2f3111770440d12044400
Anit <anittankandasamy@gmail.com>
2022-11-06 03:17:37 -0500

    added test for back functionality and created empty back function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial)
+  const [mode, setMode] = useState(initial);
 
   const transition = newMode => setMode(newMode);
+  const back = () => {};
 
   return {
     mode,
-    transition
+    transition,
+    back
   }
 }
