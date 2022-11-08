65d202d396292009a2b219c26eea15692c737281
Anit <anittankandasamy@gmail.com>
2022-11-07 22:41:42 -0500

    created save function in appt component calls bookinterview function, fixed error in

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -12,26 +12,37 @@ const SHOW = "SHOW";
 const CREATE = "CREATE";
 
 export default function Appointment(props) {
+
+  const { interview, time, interviewers, bookInterview, id } = props;
+
+  const save = (name, interviewer) => {
+    const interview = {
+      student: name,
+      interviewer,
+    };
+    bookInterview(id, interview);
+  }
   
   const { mode, transition, back } = useVisualMode(
-    props.interview ? SHOW : EMPTY
+    interview ? SHOW : EMPTY
   )
 
   return (
     
     <article className="appointment">
-    <Header time={props.time} />
+    <Header time={time} />
     {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
     {mode === SHOW && (
       <Show 
-      student={props.interview.student}
-      interviewer={props.interview.interviewer.name}
+      student={interview.student}
+      interviewer={interview.interviewer.name}
       />
     )}
     {mode === CREATE && (
       <Form 
-      interviewers={props.interviewers} 
-      onCancel={() => back()}
+      interviewers={interviewers} 
+      onCancel={back}
+      onSave={save}
       />
     )}
     </article>
