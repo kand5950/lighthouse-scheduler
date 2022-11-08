48d2dad2ec486e2c9f207bec32a93b6415e1fea1
Anit <anittankandasamy@gmail.com>
2022-11-08 15:45:17 -0500

    testing optional library prop-types passing an error

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,6 +2,8 @@ import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import 'components/InterviewerList.scss';
 
+import PropTypes from 'prop-types';
+
 export default function InterviewerList(props) {
 
   const interviewerList = props.interviewers.map(interviewer => {
@@ -24,3 +26,7 @@ export default function InterviewerList(props) {
   );
   
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
