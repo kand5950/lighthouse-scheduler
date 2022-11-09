21d4a24e67346a823dc0eb771f08d775f8125c14
Anit <anittankandasamy@gmail.com>
2022-11-09 08:28:40 -0500

    fixed minor mistake

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -7,21 +7,17 @@ export default function InterviewerListItem(props) {
   const interviewerClass = classNames('interviewers__item', 
   {'interviewers__item--selected' : props.selected});
 
-  const formatSpots = (props) => {
-    const spots = props.selected ? props.name : "";
-    return spots;
-  }
+  
 
 
   return (
-    <li onClick={props.setInterviewer} 
-    className={interviewerClass}>
-  <img
-    className={"interviewers__item-image"}
-    src={props.avatar}
-    alt={props.name}
-  />
-  {formatSpots(props)}
-</li>
+    <li onClick={props.setInterviewer} className={interviewerClass}>
+      <img
+        className={"interviewers__item-image"}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
   );
 }
