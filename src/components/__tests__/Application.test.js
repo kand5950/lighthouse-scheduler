d9ea7052aacd81cd463c1bad961210516a6c7378
Anit <anittankandasamy@gmail.com>
2022-11-09 09:37:28 -0500

    included put in axios mock so tests will pass, passed.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -23,18 +23,18 @@ describe("Application", () => {
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
-    const appointment = getAllByTestId(container, "appointment")[0];
-    
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
     fireEvent.click(getByAltText(appointment, "Add"));
 
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
-    
+
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
     fireEvent.click(getByText(appointment, "Save"));
-
 
     console.log(prettyDOM(container));
   })
