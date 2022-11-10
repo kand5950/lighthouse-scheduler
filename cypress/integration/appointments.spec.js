d31ae605df446c54a9164f9c360e005713b84480
Anit <anittankandasamy@gmail.com>
2022-11-09 19:28:48 -0500

    implement cypress test should edit an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -17,4 +17,15 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+
+  it("should edit an interview" , () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+    cy.get("[data-testid=student-name-input").clear().type("Anit");
+    cy.get("[alt='Tori Malcolm']").click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Anit");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
 });
