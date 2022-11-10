572eaac97e0be82c42655ef43089f42bab16135c
Anit <anittankandasamy@gmail.com>
2022-11-09 19:37:44 -0500

    implement cypress test should cancel an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -28,4 +28,17 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Anit");
     cy.contains(".appointment__card--show", "Tori Malcolm");
   });
+
+  it.only("should cancel an interview" , () => {
+    cy.get("[alt=Delete]")
+      .first()
+      .click({ force: true });
+    cy.contains("Confirm").click()
+    cy.contains("Deleting").should("exist")
+    cy.contains("Deleting").should("not.exist")
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
+
+
+  });
 });
